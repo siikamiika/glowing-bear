5bfd2e44132463b385ec0c67ffe70f273f349945
Mikko Perttunen <cyndis@kapsi.fi>
2015-01-29 20:12:11 +0200

    Disable plugins

--- js/plugins.js
+++ js/plugins.js
@@ -392,7 +392,8 @@ plugins.factory('userPlugins', function() {
     tweetPlugin.name = 'Tweet';
 
     return {
-        plugins: [youtubePlugin, dailymotionPlugin, allocinePlugin, imagePlugin, spotifyPlugin, cloudmusicPlugin, googlemapPlugin, asciinemaPlugin, yrPlugin, gistPlugin, tweetPlugin]
+//        plugins: [youtubePlugin, dailymotionPlugin, allocinePlugin, imagePlugin, spotifyPlugin, cloudmusicPlugin, googlemapPlugin, asciinemaPlugin, yrPlugin, gistPlugin, tweetPlugin]
+        plugins: []
     };
 
 
