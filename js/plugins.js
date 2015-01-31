712869df9390163e1cac16f401f1f531d3c8fe90
Mikko Perttunen <cyndis@kapsi.fi>
2015-01-31 21:26:44 +0200

    Enable image plugin

--- js/plugins.js
+++ js/plugins.js
@@ -393,7 +393,7 @@ plugins.factory('userPlugins', function() {
 
     return {
 //        plugins: [youtubePlugin, dailymotionPlugin, allocinePlugin, imagePlugin, spotifyPlugin, cloudmusicPlugin, googlemapPlugin, asciinemaPlugin, yrPlugin, gistPlugin, tweetPlugin]
-        plugins: []
+        plugins: [imagePlugin]
     };
 
 
